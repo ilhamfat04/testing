4df5278fac825919ecffaa7c71981a8906e546c0
ilham fathullah <ilhamfathullah04@gmail.com>
2022-07-15 10:44:30 +0700

    Day 4

--- connection/db.js
+++ connection/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 
 // Setup connection
 const dbPool = new Pool({
-    database: 'blog_batch38',
+    database: 'blog_batch37',
     port: 5432,
     user: 'postgres',
     password: 'root'
